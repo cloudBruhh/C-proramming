ace546c0e06b36bad8938d611269d36e24033a8e
cloudBruhh <ashwinthapa34009@gmail.com>
2026-01-20 10:44:54 +0545

    Add files via upload

---
+++ pattern2.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main()
+{
+    int n = 10, i, j;
+    printf("Pattern:\n");
+    for (i = 1; i <= 5; i++)
+    {
+        for (j = 1; j <= i; j++)
+        {
+            printf("%d \t", n);
+            n = n + 10;
+        }
+        printf("\n");
+    }
+}
